9ccdeb26fb9d37a1fe0864e40296d8a6fd51badf
estrattonbailey <ericstrattonbailey@gmail.com>
2022-01-05 10:23:40 -0600

    presto

--- presta.config.js
+++ presta.config.js
@@ -1,4 +1,4 @@
-import { createPlugin as sourceFs } from '@presta/source-filesystem'
+import sourceFs from '@presta/source-filesystem'
 
 export const files = ["pages/*.jsx"];
 export const plugins = [sourceFs()]
