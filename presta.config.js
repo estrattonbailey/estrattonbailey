7c082730c7fc0bdf4e05ec2193ce5b661f3b9ead
estrattonbailey <ericstrattonbailey@gmail.com>
2022-01-06 21:35:46 -0600

    ppesto

--- presta.config.js
+++ presta.config.js
@@ -1,4 +1,5 @@
 import sourceFs from '@presta/source-filesystem'
+import netlify from '@presta/adapter-netlify'
 
 export const files = ["pages/*.jsx", "pages/*.tsx"];
-export const plugins = [sourceFs()]
+export const plugins = [sourceFs(), netlify()]
